fa4304297812954f47d5f8b5a4cfb422557bb5e2
Yuan Gao <gaoyuanbj@yahoo.com>
2019-04-03 17:02:28 -0400

    Solve the bug for CORS policy
    https://stackoverflow.com/questions/46258449/cors-error-requests-are-only-supported-for-protocol-schemes-http-etc

--- front-end/js/controllers/httpService.js
+++ front-end/js/controllers/httpService.js
@@ -1,11 +1,11 @@
 app.service('httpService', function ($http) {
     function getTeamsInfo () {
-        return $http.get("localhost:8080/api/teambg");
+        return $http.get("http://localhost:8080/api/teambg");
     }
 
     return {
-    
+
         getTeamsInfo : getTeamsInfo
-        
+
     };
 });
