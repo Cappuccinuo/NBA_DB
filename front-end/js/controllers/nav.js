29455d8dbfd2a70035a8b695ebe74f553fb6d595
EdenSSS <469294531@qq.com>
2019-04-18 23:36:07 -0400

    front end final

--- front-end/js/controllers/nav.js
+++ front-end/js/controllers/nav.js
@@ -1,12 +1,4 @@
 /* global app */
 app.controller('navCtrl', function($scope, $rootScope, $location, $state) {
 
-$scope.logOut = function(){
-   $rootScope.loggedIn = false;
-   $rootScope.logInName = "";
-   $rootScope.logInId = "";
-   $location.path('home');
-   $state.reload();
- };
-
 });
